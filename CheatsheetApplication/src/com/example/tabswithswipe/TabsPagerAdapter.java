0bfb809492012fe0f0187b02e178ff59174e0d94
neahpanilag <neahpanilag@gmail.com>
2014-10-03 15:51:36 -0700

    Tab with Swipe
    implementation proposal

---
+++ CheatsheetApplication/src/com/example/tabswithswipe/TabsPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.tabswithswipe;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+	 
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+ 
+    @Override
+    public Fragment getItem(int index) {
+ 
+        switch (index) {
+        case 0:
+            // Top Rated fragment activity
+            return new HtmlFragment();
+        case 1:
+            // Games fragment activity
+            return new CssFragment();
+        case 2:
+            // Movies fragment activity
+            return new JqueryFragment();
+        }
+ 
+        return null;
+    }
+ 
+    @Override
+    public int getCount() {
+        // get item count - equal to number of tabs
+        return 3;
+    }
+ 
+}
